f189d5a6a5bf0c31a440efa72ccdc86b6994ae1e
rionando <87197820+rionando@users.noreply.github.com>
2021-12-04 13:01:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function moreabout() {
+    var i = document.getElementById("writer");
+    if (
+        i.style.display === "none") {
+        i.style.display = "block";
+    } else {
+        i.style.display = "none";
+    }
+}
